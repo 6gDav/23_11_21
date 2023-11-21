09e9d2b1a957022f0e9d4399621d907876088134
6gDav <118929714+6gDav@users.noreply.github.com>
2023-11-21 10:17:22 +0100

    minden ujra dolgozva

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-let numberContainer = document.getElementById("gamearea");
+// script.js
 
-for (let i = 1; i <= 12; i++) {
-    numberContainer.innerHTML += 'A' + "<br>";
+const container = document.getElementById('grid-container');
+const numbers = [];
+let clickedNumbers = [];
+let startTime; // Az időmérés kezdeti időpontja
+
+// Generálj véletlenszerű számokat 0-tól 11-ig
+for (let i = 0; i < 12; i++) {
+  numbers.push(i);
 }
-numberContainer.innerHTML += "<hr>";
-for (let j = 1; j <= 12; j++) {
-    numberContainer.innerHTML += 'A' + " ";
+
+// Keverd össze a számokat
+numbers.sort(() => Math.random() - 0.5);
+
+// Hozz létre div elemeket a véletlenszerű számokkal
+for (let i = 0; i < 12; i++) {
+  const box = document.createElement('div');
+  box.classList.add('box');
+  box.innerHTML = numbers[i];
+
+  // Adj hozzá egy eseménykezelőt a kattintásra
+  box.addEventListener('click', function() {
+    // Ellenőrizd a kiválasztott számot a sorrendben
+    if (parseInt(box.innerHTML) === clickedNumbers.length) {
+      // Az első kattintásnál kezdd el az időmérést
+      if (clickedNumbers.length === 0) {
+        startTime = new Date();
+      }
+
+      // A szám helyes, hozzáadhatjuk a clickedNumbers tömbhöz
+      clickedNumbers.push(parseInt(box.innerHTML));
+      // Állítsd be a láthatóságot a kiválasztott doboznál
+      box.style.visibility = 'hidden';
+
+      // Ellenőrizd, hogy az összes számot megnyomták-e sorrendben
+      if (clickedNumbers.length === numbers.length) {
+        // Minden számot megnyomtak, számold ki az eltelt időt
+        const endTime = new Date();
+        const elapsedTime = (endTime - startTime) / 1000; // Másodpercben
+
+        // Játék vége, mutasd az időt és indítsd újra a játékot
+        alert(`Gratulálok! Nyertél! Idő: ${elapsedTime} másodperc`);
+        location.reload(); // Oldal újratöltése
+      }
+    } else {
+      // A szám rossz helyen van, játék újraindítása
+      alert('Rossz sorrend! Próbáld újra!');
+      location.reload(); // Oldal újratöltése
+    }
+  });
+
+  container.appendChild(box);
 }
