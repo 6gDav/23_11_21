4908683e1232f872ce9f29e8528feb996b2a01f0
Dávid <david>
2023-11-21 08:10:58 +0100

    change

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let numberContainer = document.getElementById("numberContainer");
 
-for (var i = 1; i <= 12; i++) {
+for (let i = 1; i <= 12; i++) {
     numberContainer.innerHTML += 'A' + "<br>";
 }
 numberContainer.innerHTML += "<hr>";
-for (var j = 1; j <= 12; j++) {
+for (let j = 1; j <= 12; j++) {
     numberContainer.innerHTML += 'A' + " ";
 }
